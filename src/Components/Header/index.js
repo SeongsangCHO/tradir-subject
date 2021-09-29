0fa0f6266f27e3eea78e4918a8f71aeb31556e7d
Seongsangcho <liasqui31@gmail.com>
2021-09-29 18:44:41 +0900

    feat : Route default setting

---
+++ src/Components/Header/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const Header = (props) => {
+  return (
+    <Container>
+      <Nav>
+        <Link to="/beerlist">비어리스트</Link>
+      </Nav>
+    </Container>
+  );
+};
+
+Header.propTypes = {};
+
+export default Header;
+
+const Container = styled.header`
+  border: 1px solid black;
+  height: 60px;
+`;
+
+const Nav = styled.nav``;
--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "Pages/Home";
+import Header from "Components/Header";
+import Beerlist from "Pages/Beerlist";
 // import ReactGA from "react-ga";
 
 const Routes = () => {
   return (
     <Router>
+      <Header />
       <Switch>
-        {/* <Route path="/home" component={Home} /> */}
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/beerlist" component={Beerlist} />
+        <Route exact path="/cartview" component={Home} />
       </Switch>
     </Router>
   );
