5f81ce8b42062200727eb1f97da03ce314834cff
Seongsangcho <liasqui31@gmail.com>
2021-09-30 17:03:07 +0900

    feat : 필터버튼 클릭여부 확인 state추가

--- src/Components/Beer/BeerFilter.js
+++ src/Components/Beer/BeerFilter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 const BeerFilter = ({ beerList }) => {
   const [abvFilterGroup, setAbvFilterGroup] = useState({});
+  const [filterClickedId, setFilterClickedId] = useState({});
 
   const sortAbvOrder = (list) => {
     const sorted = list.sort((a, b) => a.abv - b.abv);
@@ -10,14 +11,17 @@ const BeerFilter = ({ beerList }) => {
   };
   const createAbvFilterGroup = () => {
     const abvGroup = {};
+    const filterId = {};
     sortAbvOrder(beerList).forEach((beer) => {
       const abvStandard = Math.floor(beer.abv / 5);
       if (!abvGroup.hasOwnProperty(abvStandard)) {
         abvGroup[abvStandard] = [];
+        filterId[abvStandard] = false;
       }
       abvGroup[abvStandard].push(beer.id);
     });
     setAbvFilterGroup({ ...abvGroup });
+    setFilterClickedId({ ...filterId });
   };
   useEffect(() => {
     createAbvFilterGroup();
@@ -30,15 +34,23 @@ const BeerFilter = ({ beerList }) => {
     return `${standard * 5}이상, ${(standard + 1) * 5}미만`;
   };
   const handleFilter = (standard) => {
-    console.log(abvFilterGroup[standard]);
+    setFilterClickedId({
+      ...filterClickedId,
+      [standard]: !filterClickedId[standard],
+    });
+    console.log("id", filterClickedId);
   };
   return (
     <Container>
       <button>All</button>
       {Object.keys(abvFilterGroup).map((standard, idx) => (
-        <button onClick={() => handleFilter(standard)} key={standard}>
+        <FilterButton
+          isClicked={filterClickedId[standard]}
+          onClick={() => handleFilter(standard)}
+          key={standard}
+        >
           {filterButtonText(parseInt(standard), idx)}
-        </button>
+        </FilterButton>
       ))}
     </Container>
   );
@@ -52,4 +64,8 @@ export default BeerFilter;
 
 const Container = styled.div`
   padding: 24px;
+`;
+
+const FilterButton = styled.button`
+  background-color: ${(props) => (props.isClicked ? "blue" : "black")};
 `;
