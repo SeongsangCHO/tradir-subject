8b9f2c756c443be7388f68d4f22a410e7dc6c090
Seongsangcho <liasqui31@gmail.com>
2021-10-01 00:19:12 +0900

    chore : 공통으로 사용되는 PageTemplate 작성

---
+++ src/Components/Common/PageTemplate.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+const PageTemplate = ({ children, ...restParams }) => {
+  return <Template {...restParams}>{children}</Template>;
+};
+
+PageTemplate.propTypes = {
+  children: PropTypes.node,
+};
+
+export default PageTemplate;
+
+const Template = styled.main`
+  width: 100%;
+  max-width: 1080px;
+  margin: 0 auto;
+  padding: 50px 30px;
+`;
