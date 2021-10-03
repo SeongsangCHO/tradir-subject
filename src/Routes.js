00472135ecafc73efdc547f9835b1821bf6fe21f
Seongsangcho <liasqui31@gmail.com>
2021-10-03 21:05:51 +0900

    feat: 잘못된 경로 -> 404 page로 라우팅

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import Home from "Pages/Home";
 import { Header } from "Components/Common";
 import BeerList from "Pages/BeerList";
 import CartView from "Pages/CartView";
+import Page404 from "Pages/Page404";
 // import ReactGA from "react-ga";
 
 const Routes = () => {
@@ -17,6 +18,7 @@ const Routes = () => {
         <Route exact path="/home" component={Home} />
         <Route exact path="/beerlist" component={BeerList} />
         <Route exact path="/cartview" component={CartView} />
+        <Route component={Page404} />
       </Switch>
     </Router>
   );
