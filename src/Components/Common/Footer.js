3db06ccd88aaecd069878654370578e89188ff1b
Seongsangcho <liasqui31@gmail.com>
2021-10-04 13:39:52 +0900

    refactor : 접근성 - github 이미지 label추가

--- src/Components/Common/Footer.js
+++ src/Components/Common/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <GitHub />
+        <GitHub aria-label="github link" />
       </a>
     </Container>
   );
